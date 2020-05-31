b7441175f8622f17873428f647ca96506366377d
JyotiJanai <31592986+JyotiJanai@users.noreply.github.com>
2020-05-31 15:59:58 -0500

    This is Addition program in python.

---
+++ Starters/Oprations.py
@@ -0,0 +1,17 @@
+# Addition
+print(10 + 15)
+
+# Subtraction
+print(10 - 5)
+
+# Multiplication
+print(10 * 3)
+
+# Division
+print(10 / 4)
+
+# Exponation
+print(4 ** 2)
+
+# Modulus
+print(10 % 4)
