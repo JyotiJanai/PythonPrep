e8b8db4d023571932af826c44d90aed4de75d3df
JyotiJanai <31592986+JyotiJanai@users.noreply.github.com>
2020-06-03 22:30:30 -0500

    This is Addition program in python.

--- Starters/ifStatement.py
+++ Starters/ifStatement.py
@@ -1,8 +1,16 @@
-a = 100
+'''a = 100
 b = 10
 if (a > b):
     print("a is greater than b")
 elif (a == b):
     print("a is equal to b")
 else:
-    print("b is greater than b")
+    print("b is greater than b")'''
+print("enter value of x")
+x = int(input())
+if x > 10:
+    print("x is greater than 10")
+if x > 20:
+    print("x is greater than 20")
+else:
+    print("x is less than 10")
