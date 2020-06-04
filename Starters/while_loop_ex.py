13a093705b818bcea86f0b81785ff5d9f5c4d682
JyotiJanai <31592986+JyotiJanai@users.noreply.github.com>
2020-06-04 14:03:59 -0500

    This is Addition program in python.

---
+++ Starters/while_loop_ex.py
@@ -0,0 +1,19 @@
+i = 1
+while i < 6:
+    i += 1
+    if (i == 3):
+        continue
+    print(i)
+
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+
+    print("i is no longer less than 6")
+
+x = 1
+while x <= 10:
+    print(x)
+    x = x + 1
