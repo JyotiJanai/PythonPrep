9a006221c34fe6a65b75263f57c7e7356c8029bc
JyotiJanai <31592986+JyotiJanai@users.noreply.github.com>
2020-06-14 00:22:27 -0500

    This is Addition program in python.

---
+++ Starters/formatstr.py
@@ -0,0 +1,8 @@
+price = input("Enter the value: ")
+txt = "The price is {} dollars"
+print(txt.format(price))
+
+age = input("Enter your age:")
+name = input("Enter yor name:")
+txt = "His name is {1}. {1} is {0} years old."
+print(txt.format(age, name))
