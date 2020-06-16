2ada9f08c1b06c8e6c6e93af621786cb912a9de6
JyotiJanai <31592986+JyotiJanai@users.noreply.github.com>
2020-06-15 22:19:56 -0500

    This is Addition program in python.

---
+++ ArrayExamples/mean.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+speed = [99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86]
+x = np.mean(speed)
+print(x)
+
+y = np.median(speed)
+print(y)
+
+from scipy import stats
+
+x = stats.mode(speed)
+print(x)
